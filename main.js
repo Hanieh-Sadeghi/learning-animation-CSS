0f74ba70950e29b9e9cc383007b290b9399ecab8
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2024-01-21 16:21:39 +0330

    add addEventListener

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-const modal = document.querySelector('.modal-container')
-const openModal = document.getElementById('openBtn')
-const closeModal = document.querySelector('.closeBtn')
+const modal = document.querySelector(".modal-container");
+const openModal = document.getElementById("openBtn");
+const closeModal = document.querySelector(".closeBtn");
 
+openModal.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = " none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = " none";
+  }
+});
